d45d10ecb133e80b1cc7c0f551587db62c85557d
DanielF <fdaniel02@gmail.com>
2020-05-12 20:18:20 +0200

    change TaskService tests to use fake instead of mock

---
+++ tests/UnitTests/NirvanaHqApi.Test/Tasks/FakeTaskApi.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using NirvanaHqApi.Api;
+using NirvanaHqApi.Models;
+
+namespace NirvanaHqApi.Test.Tasks
+{
+    internal class FakeTaskApi : ITaskApiService
+    {
+        private List<NirvanaTask> _tasks;
+
+        public FakeTaskApi(List<NirvanaTask> tasks)
+        {
+            _tasks = tasks;
+        }
+
+        public Task<bool> CreateTask(NirvanaTask task)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> CreateTasks(List<NirvanaTask> tasks)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<NirvanaTask>> GetTasksFromServer()
+        {
+            return Task.FromResult(_tasks);
+        }
+    }
+}
